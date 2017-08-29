1d7459f9589655042073047cfc6d510bff429e52
Ben Cline <benkahncline@gmail.com>
2017-08-28 21:04:49 -0700

    adding some old versions

--- bens_additions/cnn.py
+++ bens_additions/cnn.py
@@ -1,5 +1,5 @@
 from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 from keras.layers import Flatten, Dense
 from keras.optimizers import Adam
@@ -14,8 +14,6 @@ width = 224
 color_mode = 'rgb'
 shuffle = True
 seed = 0
-training_steps_per_epoch = 1 #*****should be n_training_images / batch_size
-validation_steps_per_epoch = 1 #*****should be n_validation_images / batch_size
 channels = 3
 data_format = 'channels_last'
 if data_format == 'channels_last':
@@ -27,14 +25,36 @@ epochs = 3
 n_classes = 2
 lr = .0001
 
+def preprocess_input_wrapper(x):
+    """
+    Source: https://nbviewer.jupyter.org/gist/embanner/6149bba89c174af3bfd69537b72bca74
+    Wrapper around keras.applications.vgg16.preprocess_input()
+    to make it compatible for use with keras.preprocessing.image.ImageDataGenerator's
+    `preprocessing_function` argument.
 
+    Parameters
+    ----------
+    x : a numpy 3darray (a single image to be preprocessed)
 
-train_datagen = image.ImageDataGenerator(
-    rescale=1./255,
+    Note we cannot pass keras.applications.vgg16.preprocess_input()
+    directly to to keras.preprocessing.image.ImageDataGenerator's
+    `preprocessing_function` argument because the former expects a
+    4D tensor whereas the latter expects a 3D tensor. Hence the
+    existence of this wrapper.
+
+    Returns a numpy 3darray (the preprocessed image).
+
+    """
+    X = np.expand_dims(x, axis=0)
+    X = preprocess_input(X)
+    return X[0]
+
+train_datagen = ImageDataGenerator(
+    preprocessing_function = preprocess_input_wrapper
     data_format = data_format)
 
-test_datagen = image.ImageDataGenerator(
-    rescale=1./255,
+test_datagen = ImageDataGenerator(
+    preprocessing_function = preprocess_input_wrapper
     data_format = data_format)
 
 train_generator = train_datagen.flow_from_directory(
@@ -53,6 +73,10 @@ validation_generator = test_datagen.flow_from_directory(
         shuffle = shuffle,
         seed = seed)
 
+training_steps_per_epoch = int(train_generator.samples / batch_size)
+validation_steps_per_epoch = int(validation_generator.samples / batch_size)
+print training_steps_per_epoch, train_generator.samples
+print validation_steps_per_epoch, validation_generator.samples
 
 
 
@@ -68,7 +92,7 @@ for layer in base_model.layers:
     layer.trainable = False
 
 model.summary()
-model.compile(optimizer=Adam(lr = lr), loss='categorical_crossentropy', metrics = metrics=['accuracy'])
+model.compile(optimizer=Adam(lr = lr), loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit_generator(
         train_generator,
         steps_per_epoch=training_steps_per_epoch,
--- bens_additions/sort_downloaded_images.py
+++ bens_additions/sort_downloaded_images.py
@@ -1,6 +1,7 @@
 import os
 import gdal
 import pandas as pd
+import numpy as np
 
 def get_value_at_point(data, geotransform, latitude, longitude):
     """Finds value of raster data at a lat and long coordinate pair.
@@ -26,7 +27,6 @@ def get_list_of_filepaths(rootdir, filetype = '.png'):
         for cur_file in files:
             filepath =  os.path.join(subdir, cur_file)
             if filepath.endswith(filetype):
-                print filepath
                 files_df = files_df.append({'filepath': filepath, 'filename': cur_file},
                             ignore_index = True)
     return files_df
@@ -40,8 +40,17 @@ def get_latitude_and_longitude_from_filename(filename):
     longitude = float(filename_components[2])
     return latitude, longitude
 
+def get_nightlights_bin(nightlights_value, array_of_bins, out_of_range_value = -1):
+    #nightlights_bin is the arg of the corresponding bin in array_of_bins
 
-def get_nightlights_data(nightlights_filepath, files_df):
+    binned_array = np.histogram(nightlights_value, array_of_bins)
+    if np.argwhere(binned_array[0] == True).size == 0:
+        nightlights_bin = out_of_range_value
+    else:
+        nightlights_bin = np.argwhere(binned_array[0] == True).reshape(-1)[0]
+    return nightlights_bin
+
+def get_nightlights_data(nightlights_filepath, files_df, nightlights_array_of_bins):
     ds = gdal.Open(nightlights_filepath)
     nightlights_data = ds.ReadAsArray()
     nightlights_geotransform = ds.GetGeoTransform()
@@ -51,39 +60,24 @@ def get_nightlights_data(nightlights_filepath, files_df):
     print 'projection: ', nightlights_projection
 
     files_df['latitude'], files_df['longitude'] =  zip(*files_df['filename'].apply(lambda x: get_latitude_and_longitude_from_filename(x)))
+    print 'added lat and lon'
     files_df['nightlights_value'] = files_df.apply(
                                 lambda x: get_value_at_point(nightlights_data,
                                                             nightlights_geotransform,
                                                             x['latitude'],
                                                             x['longitude']), axis = 1)
+    print 'added nightlights value'
+    files_df['nightlights_bin'] = files_df['nightlights_value'].apply(lambda x: get_nightlights_bin(x, nightlights_array_of_bins))
     return files_df
-
-
-'''
-def get_nightlights_data(nightlights_filepath, list_of_filepaths):
-    ds = gdal.Open(nightlights_filepath)
-    nightlights_data = ds.ReadAsArray()
-    nightlights_geotransform = ds.GetGeoTransform()
-    nightlights_projection = ds.GetProjection()
-    print 'shape of nightlights data: ', nightlights_data.shape
-    print 'geotransform: ', nightlights_geotransform
-    print 'projection: ', nightlights_projection
-
-    list_of_nightlights_values = []
-    for filepath in list_of_filepaths:
-        filename = get_filename_from_filepath(filepath)
-        latitude, longitude = get_latitude_and_longitude_from_filename(filename)
-        nightlights_value = get_value_at_point(data, geotransform, latitude, longitude)
-        list_of_nightlights_values.append(nightlights_value)
-    return list_of_nightlights_values
-'''
 
 
 if __name__ == '__main__':
     rootdir = 'data/malawi_images'
     nightlights_filepath = '../../DMSP/F182013.v4c_web.avg_vis.tif'
+    nightlights_array_of_bins = np.array([0,3,35,63]) #based on supplemental methods of jean et al 2016
+    nightlights_df_filepath = 'nightlights_df.csv'
 
     files_df= get_list_of_filepaths(rootdir)
-    print files_df
-    nightlights_df = get_nightlights_data(nightlights_filepath, files_df)
-    print nightlights_df
+    nightlights_df = get_nightlights_data(nightlights_filepath, files_df, nightlights_array_of_bins)
+    nightlights_df.to_csv(nightlights_df_filepath)
+    print np.histogram(nightlights_df['nightlights_bin'].values, range(-1, nightlights_array_of_bins.size+1))
